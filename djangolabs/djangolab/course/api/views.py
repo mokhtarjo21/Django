eb188173b6a06abd1682235dcf71dec48ddd381d
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-03-18 15:24:58 +0200

    end day 5

--- djangolabs/djangolab/course/api/views.py
+++ djangolabs/djangolab/course/api/views.py
@@ -1,5 +1,5 @@
 from django.template.context_processors import request
-
+from rest_framework.decorators import api_view
 from .serlizer import C_serlizer
 from rest_framework import generics
 from course.models import Course
@@ -8,9 +8,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+@api_view(['PUT'])
 def update(request,id):
-    Course1=get_object_or_404(Course,id=id)
-    if request.method=='put':
+        Course1=get_object_or_404(Course,id=id)
         objserlized=C_serlizer(data=request.data,instance=Course1)
       
         if(objserlized.is_valid()):
@@ -19,14 +19,10 @@ def update(request,id):
             data=objserlized.data,
             status=status.HTTP_200_OK)
         else:
-         return Response(data=
+            return Response(data=
                          {
                             'errors':objserlized.errors
                          })
-    return Response(data=
-                    {
-                        'errors':'invalid request'
-                    })
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = C_serlizer
